75d7108f1ed3d635a0f818f783ef68d17ba451e0
Kun Huang <gareth@openstacker.org>
2015-09-16 15:52:07 +0800

    remove single quota in mysq query

--- ceilometer/mysqllive.sh
+++ ceilometer/mysqllive.sh
@@ -13,12 +13,12 @@ old_log_file=`mysql -e "SELECT @@$log_file_var" | grep -v $log_file_var | grep -
 old_log_switch=`mysql -e "SELECT @@$log_switch_var" | grep -v $log_switch_var | grep -v '\-\-\-\-\-'`
 
 # TODO enable mysql log
-mysql -e "SET GLOBAL $log_file_var= '$log_file';"
-mysql -e "SET GLOBAL $log_switch_var= 'ON';"
+mysql -e "SET GLOBAL $log_file_var = '$log_file';"
+mysql -e "SET GLOBAL $log_switch_var = ON;"
 
 sudo tailf $log_file
 
 # TODO reset everything back
-mysql -e "SET GLOBAL $log_switch_var= '$old_log_switch';"
-mysql -e "SET GLOBAL $log_file_var= '$old_log_file';"
+mysql -e "SET GLOBAL $log_switch_var = $old_log_switch;"
+mysql -e "SET GLOBAL $log_file_var = '$old_log_file';"
 sudo rm $log_file
