d8c0d275a94446dea6f8474e668d98210e552814
Kun Huang <gareth@openstacker.org>
2015-09-16 15:26:53 +0800

    revese default log file path and reset back in the end

--- ceilometer/mysqllive.sh
+++ ceilometer/mysqllive.sh
@@ -8,12 +8,15 @@
 
 log_file_var=general_log_file
 log_switch_var=general_log
+log_file=/tmp/mysqllive.log
 # TODO enable mysql log
 mysql -e "SET GLOBAL $log_switch_var= 'ON';"
 
 # TODO tail the logfile
-logfile=`mysql -e "SELECT @@$log_file_var" | grep -v general_log_file | grep -v '\-\-\-\-\-'`
-tailf $logfile
+old_log_file=`mysql -e "SELECT @@$log_file_var" | grep -v general_log_file | grep -v '\-\-\-\-\-'`
+mysql -e "SET GLOBAL $log_file_var= '$log_file';"
 
-# TODO disable mysql log
+
+# TODO reset everything back
 mysql -e "SET GLOBAL $log_switch_var= 'OFF';"
+mysql -e "SET GLOBAL $log_file_var= '$old_log_file';"
