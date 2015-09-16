4941cc6aec54bd468bf6e304ece13dbda4641bbd
Kun Huang <gareth@openstacker.org>
2015-09-16 15:40:18 +0800

    fix steps of enabling log and reseting

--- ceilometer/mysqllive.sh
+++ ceilometer/mysqllive.sh
@@ -9,14 +9,16 @@
 log_file_var=general_log_file
 log_switch_var=general_log
 log_file=/tmp/mysqllive.log
+old_log_file=`mysql -e "SELECT @@$log_file_var" | grep -v $log_file_var | grep -v '\-\-\-\-\-'`
+old_log_switch=`mysql -e "SELECT @@$log_switch_var" | grep -v $log_switch_var | grep -v '\-\-\-\-\-'`
+
 # TODO enable mysql log
+mysql -e "SET GLOBAL $log_file_var= '$log_file';"
 mysql -e "SET GLOBAL $log_switch_var= 'ON';"
 
-# TODO tail the logfile
-old_log_file=`mysql -e "SELECT @@$log_file_var" | grep -v general_log_file | grep -v '\-\-\-\-\-'`
-mysql -e "SET GLOBAL $log_file_var= '$log_file';"
-
+sudo tailf $log_file
 
 # TODO reset everything back
-mysql -e "SET GLOBAL $log_switch_var= 'OFF';"
+mysql -e "SET GLOBAL $log_switch_var= '$old_log_switch';"
 mysql -e "SET GLOBAL $log_file_var= '$old_log_file';"
+sudo rm $log_file
