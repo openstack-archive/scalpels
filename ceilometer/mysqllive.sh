50f8a9667c1ca1a1ccee236da217653239ec6ad0
Kun Huang <gareth@openstacker.org>
2015-09-17 10:23:16 +0800

    add awk part

--- ceilometer/mysqllive.sh
+++ ceilometer/mysqllive.sh
@@ -12,13 +12,28 @@ log_file=/tmp/mysqllive.log
 old_log_file=`mysql -e "SELECT @@$log_file_var" | grep -v $log_file_var | grep -v '\-\-\-\-\-'`
 old_log_switch=`mysql -e "SELECT @@$log_switch_var" | grep -v $log_switch_var | grep -v '\-\-\-\-\-'`
 
-# TODO enable mysql log
 mysql -e "SET GLOBAL $log_file_var = '$log_file';"
 mysql -e "SET GLOBAL $log_switch_var = ON;"
 
-sudo tailf $log_file
+# TODO use awk /reg/ statement instead
+sudo tailf $log_file | awk '{
+if ( $1 + 0 != $1 )
+    print $0;
+else if ( $1 + 0 == $1 && $2 == "Query" && $3 == "SELECT" && $4 == "1" )
+    # 0000 Query Limit 1
+    ;
+else if ( $1 + 0 == $1 && $2 == "Query" && $3 == "COMMIT" )
+    # 0000 Query COMMIT
+    ;
+else if ( $1 + 0 == $1 && $2 == "Query" && $3 == "ROLLBACK" )
+    # 0000 Query ROLLBACK
+    ;
+else
+    # real queries
+    { $1=$2=""; print $0; }
+}
+'
 
-# TODO reset everything back
 mysql -e "SET GLOBAL $log_switch_var = $old_log_switch;"
 mysql -e "SET GLOBAL $log_file_var = '$old_log_file';"
 sudo rm $log_file
