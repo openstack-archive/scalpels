46a2283c935176184fc881133f0b7d09db51efe4
Kun Huang <gareth@openstacker.org>
2015-09-17 10:32:13 +0800

    add trap INT

--- ceilometer/mysqllive.sh
+++ ceilometer/mysqllive.sh
@@ -11,6 +11,12 @@ log_switch_var=general_log
 log_file=/tmp/mysqllive.log
 old_log_file=`mysql -e "SELECT @@$log_file_var" | grep -v $log_file_var | grep -v '\-\-\-\-\-'`
 old_log_switch=`mysql -e "SELECT @@$log_switch_var" | grep -v $log_switch_var | grep -v '\-\-\-\-\-'`
+trap ':' INT
+
+echo -------------------------------------
+echo reserve $log_file_var: $log_file
+echo reserve $log_switch_var: ON
+echo -------------------------------------
 
 mysql -e "SET GLOBAL $log_file_var = '$log_file';"
 mysql -e "SET GLOBAL $log_switch_var = ON;"
@@ -34,6 +40,10 @@ else
 }
 '
 
+echo reset $log_file_var to $old_log_file
+echo reset $log_switch_var to $old_log_switch
 mysql -e "SET GLOBAL $log_switch_var = $old_log_switch;"
 mysql -e "SET GLOBAL $log_file_var = '$old_log_file';"
+
+echo remove $log_file
 sudo rm $log_file
