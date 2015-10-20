e59243e153c1984cac87e86ef06c2ef12e9acd72
Kun Huang <gareth@openstacker.org>
2015-10-20 15:39:06 +0800

    add simple python framework

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# Author: Kun Huang <academicgareth@gmail.com>
+
+import setuptools
+
+setuptools.setup(
+        setup_requires=['pbr'],
+        pbr=True)
