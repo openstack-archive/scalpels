1f998c317f51b3423fddd781148f67aecc1397cc
Kun Huang <gareth@openstacker.org>
2015-10-25 22:22:40 +0800

    add sca command in pre-test hook
    Change-Id: I63c9065a6ca8839e0138d3fc0b42001409958fa0

--- tests/ci/pre-test-hook.sh
+++ tests/ci/pre-test-hook.sh
@@ -2,3 +2,15 @@
 # Author: Kun Huang <academicgareth@gmail.com>
 
 echo "hello, pre-test-hook"
+
+echo "runing rabbit tracer"
+sca start -a rabbit
+sca report
+
+echo "running traffic tracer"
+sca start -a traffic
+sca report
+
+echo "running rpc tracer"
+sca start -a rpc
+sca report
