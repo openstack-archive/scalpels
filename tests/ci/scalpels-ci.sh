3a9670211d7ae7a4179612a5f32372fa521de0d0
Kun Huang <gareth@openstacker.org>
2015-10-27 11:29:06 +0800

    run port traffice tracer
    Change-Id: I7e237bdb5472ad0396780a6cd600376b1df98cd4

--- tests/ci/scalpels-ci.sh
+++ tests/ci/scalpels-ci.sh
@@ -10,12 +10,25 @@ sudo ps axf
 sudo env
 env
 
+sudo iptables -A INPUT -p tcp --dport 5672
+sudo iptables -L INPUT -x -n -v
+
 echo "running load"
 source /opt/stack/new/devstack/openrc admin admin
 sca load --storm
 
-echo "running rpc tracer"
 sca start -a rpc
+echo "running rpc tracer"
+sudo iptables -L INPUT -x -n -v
+sudo iptables -L INPUT -x -n -v
+sudo iptables -L INPUT -x -n -v
+sudo iptables -L INPUT -x -n -v
+sudo iptables -L INPUT -x -n -v
+sudo iptables -L INPUT -x -n -v
+sudo iptables -L INPUT -x -n -v
+sudo iptables -L INPUT -x -n -v
+sudo iptables -L INPUT -x -n -v
+sudo iptables -D INPUT -p tcp --dport 5672
 sca report
 
 echo "running rabbit tracer"
