bc3de6002d8cd5e2cc020a7df39e9541e0b471c6
Kun Huang <gareth@openstacker.org>
2015-10-30 23:39:41 +0800

    enable dtrace feature in python
    Change-Id: I30b1a140b50bc2ea2d558eece48447fb51ef5ce7
    Depends-On: I51b6c5494140cd408e3b6ed1280e89539c73889a

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -20,12 +20,74 @@ function configure_scalpels {
     echo "nothing need config now."
 }
 
+function install_systemtap {
+    old_dir=`pwd`
+    cd $DATA_DIR
+    wget https://sourceware.org/systemtap/ftp/releases/systemtap-2.5.tar.gz
+    tar zxf systemtap-2.5.tar.gz
+    wget https://fedorahosted.org/releases/e/l/elfutils/0.158/elfutils-0.158.tar.bz2
+    tar jxf elfutils-0.158.tar.bz2
+    cd systemtap-2.5
+    ./configure  "--with-elfutils=$DATA_DIR/elfutils-0.158" --prefix=/usr/
+    make
+    sudo make install
+    sudo stap -e 'probe begin { printf("Hello, World!\n"); exit() }'
+    cd $old_dir
+}
+
+function check_python_and_pip {
+    which python
+    which pip
+    python -c "import sys; print sys.path"
+    ls /usr/local/lib/python2.7/dist-packages
+    python -c "import pip; print pip.__file__"
+}
+
+function install_dtrace_python {
+    old_dir=`pwd`
+    cd $DATA_DIR
+
+    wget https://github.com/python/cpython/archive/ad609d460a207bc12ca83b43ab764ea58bd013ab.zip -O cpython.zip
+    wget https://raw.githubusercontent.com/pyKun/openstack-systemtap-toolkit/master/cpython-patch/python_dtrace-2_7_9-enhanced.patch -O python_dtrace-2_7_9-enhanced.patch
+    unzip cpython.zip
+    mv ./cpython-ad609d460a207bc12ca83b43ab764ea58bd013ab ./cpython
+    cd cpython
+
+    git init
+    git apply ../python_dtrace-2_7_9-enhanced.patch
+
+    check_python_and_pip
+    #sudo rm -rf /usr/local/lib/python2.7
+    autoconf
+    #./configure "--prefix=$DATA_DIR/cpython_build/" '--with-dtrace' '--enable-ipv6' '--enable-unicode=ucs2' '--with-dbmliborder=bdb:gdbm' '--with-system-expat' '--with-system-ffi' '--with-fpectl'
+    ./configure "--prefix=/usr/local/" '--with-dtrace' '--enable-ipv6' '--enable-unicode=ucs2' '--with-dbmliborder=bdb:gdbm' '--with-system-expat' '--with-system-ffi' '--with-fpectl'
+    make -j && sudo make install
+
+    cd $DATA_DIR
+    sudo stap -l 'process("/usr/local/bin/python").mark("*")'
+    export PYTHONPATH=/usr/local/lib/python2.7/dist-packages
+
+    check_python_and_pip
+    sed 's/pip_install -U setuptools//' $TOP_DIR/tools/install_pip.sh > $TOP_DIR/tools/fixed_install_pip.sh
+    sudo chmod +x $TOP_DIR/tools/fixed_install_pip.sh
+    sudo PYPI_ALTERNATIVE_URL=${PYPI_ALTERNATIVE_URL:-""} $TOP_DIR/tools/fixed_install_pip.sh
+    check_python_and_pip
+    env
+    python -c "import pip; print pip.__file__"
+    pip_version=$(python -c "import pip;print(pip.__version__.strip('.')[0])")
+    pip_install -U virtualenv
+    cd $old_dir
+}
+
 # check for service enabled
 if is_service_enabled scalpels; then
 
     if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
         # Set up system services
         echo_summary "Configuring system services scalpels"
+        install_package autoconf automake gcc m4
+        install_systemtap
+        install_dtrace_python
 
     elif [[ "$1" == "stack" && "$2" == "install" ]]; then
         # Perform installation of service source
