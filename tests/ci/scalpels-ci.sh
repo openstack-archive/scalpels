622527ab2ed902ea9cb0501879080d38a7a0915b
Kun Huang <gareth@openstacker.org>
2015-10-30 17:33:41 +0800

    add scripts to dl linux headers
    Change-Id: I47ba81ca1f9a037a347bfd315cba47e705580b3a

--- tests/ci/scalpels-ci.sh
+++ tests/ci/scalpels-ci.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 # Author: Kun Huang <academicgareth@gmail.com>
 
+
 echo "Hello, scalpels ci"
 sca setup -d /opt/stack/data/scalpels/scripts
 
@@ -10,28 +11,18 @@ sudo ps axf
 sudo env
 env
 
-echo basic agents
-sca start -a rpc -a rabbit -a traffic
+sudo apt-search kernel-image # find a lastest one, 3.19.0-30-generic for example
+# reboot your system to ensure you are running the new kernel
+codename=$(lsb_release -c | awk  '{print $2}')
+sudo tee /etc/apt/sources.list.d/ddebs.list << EOF
+deb http://ddebs.ubuntu.com/ ${codename}      main restricted universe multiverse
+deb http://ddebs.ubuntu.com/ ${codename}-security main restricted universe multiverse
+deb http://ddebs.ubuntu.com/ ${codename}-updates  main restricted universe multiverse
+deb http://ddebs.ubuntu.com/ ${codename}-proposed main restricted universe multiverse
+EOF
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ECDCAD72428D7C01
+sudo apt-get update
+sudo apt-get install -y linux-image-$(uname -r)-dbgsym
 
-source /opt/stack/new/devstack/openrc admin admin
-sca load --storm
-sleep 10
-sca stop
-
-echo waiting agent write data into db before report
-sleep 20
-
-sca report
-
-sca result --list
-
-echo test html
-sca start -a rpc
-sca load --storm
-sleep 120
-sca stop
-
-for i in `sca result --list --short | tail -n2`; do
-    sca result $i --html > $BASE/logs/scalpels-result-$i.html
-done
-sca report --html > $BASE/logs/scalpels-report.html
+sudo apt-get install -y systemtap gcc
+sudo stap -l 'kernel.function("*")'
