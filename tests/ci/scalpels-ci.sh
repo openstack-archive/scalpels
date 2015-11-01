c908f2cb0e1fde2b795ba138a5aac68ddffc178e
Kun Huang <gareth@openstacker.org>
2015-11-01 16:00:06 +0800

    re-structure ci script
    Change-Id: I89635cc71aead11b9605ae681d2c2dc5f4d28b9a

--- tests/ci/scalpels-ci.sh
+++ tests/ci/scalpels-ci.sh
@@ -4,34 +4,41 @@
 echo "Hello, scalpels ci"
 sca setup -d /opt/stack/data/scalpels/scripts
 
-# debug messages
-sudo netstat -nltp
-sudo ps axf
-sudo env
-env
+functions debug_msg (){
+    sudo netstat -nltp
+    sudo ps axf
+    sudo env
+    env
+}
 
-echo basic agents
-sca start -a rpc -a rabbit -a traffic
+functions basic_test (){
+    sca start -a rpc -a rabbit -a traffic
 
-source /opt/stack/new/devstack/openrc admin admin
-sca load --storm
-sleep 10
-sca stop
+    source /opt/stack/new/devstack/openrc admin admin
+    sca load --storm
+    sleep 10
+    sca stop
 
-echo waiting agent write data into db before report
-sleep 20
+    echo waiting agent write data into db before report
+    sleep 20
 
-sca report
-
-sca result --list
+    sca report
+    sca result --list
+}
 
 echo test html
-sca start -a rpc
-sca load --storm
-sleep 120
-sca stop
+functions report_html_test (){
+    sca start -a rpc
+    sca load --storm
+    sleep 120
+    sca stop
 
-for i in `sca result --list --short | tail -n2`; do
-    sca result $i --html > $BASE/logs/scalpels-result-$i.html
-done
-sca report --html > $BASE/logs/scalpels-report.html
+    for i in `sca result --list --short | tail -n2`; do
+        sca result $i --html > $BASE/logs/scalpels-result-$i.html
+    done
+    sca report --html > $BASE/logs/scalpels-report.html
+}
+
+debug_msg
+basic_test
+report_html_test
