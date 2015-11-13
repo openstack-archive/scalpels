1163eae56c815243b9328aea228e7a4b0ce8f9df
Kun Huang <gareth@openstacker.org>
2015-11-13 21:58:18 +0800

    add log for rpc server
    Change-Id: I94bf597d3da4a3876290c4a9eab9948432d98044

--- scalpels/cmd/tracer.py
+++ scalpels/cmd/tracer.py
@@ -29,6 +29,7 @@ def read_from_ag(ag):
 def main():
     task_uuid, ag = sys.argv[1], sys.argv[2]
     cmd = read_from_ag(ag)
+    print "[LOG] running CMD: %s" % cmd
 
     worker = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
     out = []
@@ -38,6 +39,7 @@ def main():
             if not len(t):
                 break
             _t = (time.time(), t.strip())
+            print "[LOG] get %s" % _t
             out.append(_t)
     except KeyboardInterrupt:
         pass
