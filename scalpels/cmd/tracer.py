81bf8eed46b8c6410e85c045da8db46abd0885f3
Kun Huang <gareth@openstacker.org>
2015-11-13 21:58:18 +0800

    add log for rpc server
    Change-Id: I94bf597d3da4a3876290c4a9eab9948432d98044

--- scalpels/cmd/tracer.py
+++ scalpels/cmd/tracer.py
@@ -29,6 +29,7 @@ def read_from_ag(ag):
 def main():
     task_uuid, ag = sys.argv[1], sys.argv[2]
     cmd = read_from_ag(ag)
+    print "[LOG] running CMD: %s" % cmd
 
     worker = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
     out = []
@@ -38,12 +39,15 @@ def main():
             if not len(t):
                 break
             _t = (time.time(), t.strip())
+            print "[LOG] get %s" % _t[1]
             out.append(_t)
     except KeyboardInterrupt:
+        print "[LOG] %s is interupted" % ag
         pass
 
     # psutil is much more professional... I have to use it instead
     # this kill is to script process
+    print "[LOG] debug"
     worker_p = psutil.Process(worker.pid)
     worker_p.send_signal(signal.SIGINT)
 
@@ -59,7 +63,9 @@ def main():
         results = copy(task.results)
         for ret in parse_func(out):
             ret = db_api.result_create(**ret)
+            print "[LOG] appending result with id %s" % ret.uuid
             results.append(ret.uuid)
+        print "[LOG] update tas with result %s" % task_uuid
         db_api.task_update(task_uuid, results=results)
         time.sleep(2)
     co.stop()
